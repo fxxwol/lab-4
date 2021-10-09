1ea5937747a4015a2593f5b82eb5e14eb4e6c948
Roksolana <protsivroksolana2004@gmail.com>
2021-10-10 02:10:08 +0300

    labs

---
+++ Lab-4.5/Lab-4.5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		if ((y <= x && y >= (x - 2) * (x - 2) - 3 && y >= 0) || (y <= -x && y >= (x - 2) * (x - 2) - 3))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+	for (int i = 0; i < 10; i++)
+	{
+		x = -1 + rand() * 6. / RAND_MAX;
+		y = -3 + rand() * 8 / RAND_MAX;
+		if ((y <= x && y >= (x - 2) * (x - 2) - 3 && y >= 0) || (y <= -x && y >= (x - 2) * (x - 2) - 3))
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+	}
+	return 0;
+}
