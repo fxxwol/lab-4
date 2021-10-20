b8997aae4e2d69958747a9b6db5d11369d34d424
Roksolana <protsivroksolana2004@gmail.com>
2021-10-20 10:27:04 +0300

    master

--- Lab-4.7.cpp
+++ Lab-4.7.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+
 using namespace std;
-int main()
-{
+
+int main() {
 	double xp, xk, x, dx, eps, a = 0, R = 0, S = 0, Pi;
 	int n = 0;
 	Pi = 4 * atan(1.0);
+
 	cout << "xp = "; cin >> xp;
 	cout << "xk = "; cin >> xk;
 	cout << "dx = "; cin >> dx;
 	cout << "eps = "; cin >> eps;
+
 	cout << fixed;
 	cout << "-------------------------------------------------" << endl;
-	cout << "|" << setw(5) << "x" << " |"
-		<< setw(10) << "exp(x)" << " |"
+	cout << "|" << setw(7) << "x" << " |"
+		<< setw(10) << "acos(x)" << " |"
 		<< setw(10) << "S" << " |"
-		<< setw(5) << "n" << " |"
-		<< endl;
+		<< setw(5) << "n" << " |" << endl;
 	cout << "-------------------------------------------------" << endl;
+
 	x = xp;
-	while (x <= xk, abs(x) < 1)
-	{
-		n = 1; 
-		a = (x*x*x)/6.; 
-		S = a;
+	while (x <= xk) {
+		n = 1;
+		a = (pow(x, 3)) / 6.;
+		S = Pi / 2. - x - a;
 		do {
 			n++;
-			R = ((n - 1) * (2 * n - 1) * (2 * n - 1) * x * x) / (n * (2 * n - 3) * (2 * n + 1));
+			R = pow((2 * n - 1) * x, 2) / (2. * n * (2 * n + 1));
 			a *= R;
-			S += Pi/2.-(x+a);
+			S -= a;
 		} while (abs(a) >= eps);
-		cout << "|" << setw(5) << setprecision(2) << x << " |"
-			<< setw(10) << setprecision(5) << exp(x) << " |"
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << acos(x) << " |"
 			<< setw(10) << setprecision(5) << S << " |"
 			<< setw(5) << n << " |"
 			<< endl;
